fa1352c73bd1e3d8afd0322fd6afedab9073142a
Abombidus <abombidus@gmail.com>
2025-12-13 02:06:09 +0100

    commit=1

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,13 +1,59 @@
 import React from "react";
+import { FaEdit } from "react-icons/fa";
+import { MdAutoDelete } from "react-icons/md";
+import "./home.css";
 
 function Home() {
+  const now = new Date();
+  const hrs = now.getHours();
+  const mins = now.getMinutes();
+  const secs = now.getSeconds();
+  const schema = `${hrs}:${mins}:${secs}`;
   return (
     <>
       <button id="btnEdit">Modifer</button>
 
-      <div>
+      <div className="entete">
         <p>Dossier</p>
-        <input className="searchBar" type="search" placeholder="Recherche" />
+
+        <div>
+          <input
+            className="searchBar"
+            type="search"
+            placeholder="🔍 Recherche....."
+          />
+        </div>
+      </div>
+
+      {/* Apres l'entete */}
+
+      <div className="afterEntete">
+        <hr />
+        <div className="contents">
+          <div className="boite">
+            <fieldset>
+              <legend>Liste de note</legend>
+              <div className="contenu">
+                <h5>title</h5>:
+                <div>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Nostrum expedita deserunt, dolor accusamus sint maxime nihil
+                  laborum, sed deleniti libero qui harum perspiciatis aut? Nam
+                  sequi itaque dolore similique culpa?
+                </div>
+                <div className="btns">
+                  <p className="time">{schema}</p>
+                  <button id="deleteTaskBtn">
+                    <MdAutoDelete />
+                  </button>
+                  <button id="editTaskBtn">
+                    <FaEdit />
+                  </button>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+        </div>
       </div>
     </>
   );
